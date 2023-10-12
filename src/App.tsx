2f735ed36251c53b14537f4c4875276da08da138
danang sukma wantara <danangsukma@gmail.com>
2023-10-12 21:54:36 +0700

    Refactor to class

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,55 @@
-import { useState } from "react";
+import { Component } from "react";
 import words from './resource/wordList.json'
 import HangmanResult from './component/HangmanResult'
 import HangmanDrawing from './component/HangmanDrawing'
 import HangmanWord from './component/HangmanWord'
-import HangmanKeyboard  from './component/HangmanKeyboard'
+import HangmanKeyboard from './component/HangmanKeyboard'
 
-function App() { 
-  const [guessWord, setGuessWord] = useState<string>(() => { 
+type HangmanState = {
+  guessWord: string;
+  guessedWords: string[];
+}
+
+class App extends Component {
+  state: HangmanState= {
+      guessWord: this.genWord(),
+      guessedWords: []
+  };
+
+  guessWord = () => {
+    this.setState({
+        guessWord: this.genWord(),
+        guessedWords: [...this.state.guessedWords, this.state.guessWord]
+    });
+
+    console.log(this.state.guessWord, this.state.guessedWords);
+  };
+
+  genWord() { 
     return words[Math.floor(Math.random() * words.length)];
-  });
-  const [guessedWords, setGuessedWord] = useState<string[]>([]);
-
-  // Temporary, discard later
-  () => { 
-    setGuessWord(guessWord);
-    setGuessedWord(guessedWords);
   }
   
-  return (
-    <div
-      style={{
-        maxWidth: '800px',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '2rem',
-        margin: '0 auto',
-        alignItems: 'center',
-      }}>
-      <HangmanResult />
-      <HangmanDrawing />
-      <HangmanWord />
-      <HangmanKeyboard />
-    </div>
-  )
+  render() {
+    return (
+      <div
+        style={{
+          maxWidth: '800px',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '2rem',
+          margin: '0 auto',
+          alignItems: 'center',
+        }}>
+        {/* <button onClick={this.guessWord}>TEST</button> */}
+        <HangmanResult />
+        <HangmanDrawing />
+        <HangmanWord />
+        <div style={{ alignSelf: 'stretch' }}>
+            <HangmanKeyboard />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App
