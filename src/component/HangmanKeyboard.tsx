f81914ec359bf8962d178524198c2d90829a2886
danang sukma wantara <danangsukma@gmail.com>
2023-10-12 21:55:23 +0700

    Add keyboard component

--- src/component/HangmanKeyboard.tsx
+++ src/component/HangmanKeyboard.tsx
@@ -1,9 +1,23 @@
 import { Component } from "react";
+import '../style/HangmanKeyboard.css'
+
+const KEYS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'f5']
 
 class HangmanKeyboard extends Component
 {
     render() { 
-        return (<div>Hangman keyboard</div>);
+        return (<div style={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(auto-fit, minmax(75px, 1fr))',
+            gap: '.5rem',
+        }}>
+            {KEYS.map(key => { 
+                return (
+                    <button className='btn' key={`key-${key}`}
+                    >{key}</button>
+                )
+            })}
+        </div>);
     }
 }
 
